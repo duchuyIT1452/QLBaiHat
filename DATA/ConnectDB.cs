a7053e91d6cdd9eeff946e9fd800e06641abc742
duchuyIT1452 <85328043+duchuyIT1452@users.noreply.github.com>
2021-08-08 11:04:56 +0700

    Đức Huy add class GetData CaSi và BaiHat

---
+++ DATA/ConnectDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA
+{
+    public class ConnectDB
+    {
+        public SqlConnection getConnect()
+        {
+            string connString = @"Data Source=DESKTOP-CMM6T7Q;Initial Catalog=DBBaiHat;Integrated Security=True";
+            return new SqlConnection(connString);
+        }
+
+        public DataTable GetTable(String sql)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = getConnect();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(dt);
+            return dt;
+        }
+        public void ExcuteNonQuery(String sql)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = getConnect();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
